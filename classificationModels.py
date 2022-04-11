b72eba9f2d7bf940f57379bfa54863b00a5cdc31
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2022-04-11 19:43:15 +0800

    knn model

--- classificationModels.py
+++
@@ -1,8 +0,0 @@
-# classificatio model to predict moons (moon dataset)
-
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-
-
